07f4aee7e9e875e95af99d64470c32cff9cde21e
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-07-30 15:06:37 +0300

    extends & blocks settings

--- project/home/urls.py
+++ project/home/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about', views.about, name="about"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
+    path('blog', views.blog, name="blog"),
+    path('contact', views.contact, name="contact"),
 ]
--- project/home/views.py
+++ project/home/views.py
@@ -8,3 +8,15 @@ def home(request):
 
 def about(request):
     return render(request, "about.html")
+
+def register(request):
+    return render(request, "register.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def blog(request):
+    return render(request, "blog.html")
+
+def contact(request):
+    return render(request, "contact.html")
