71eeac73f0b65d04cdee321cd03a11c9eb3066c3
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-08-01 16:18:11 +0300

    home page articles

--- project/home/views.py
+++ project/home/views.py
@@ -1,10 +1,18 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from articles.models import *
 # Create your views here.
 
 
 def home(request):
-    return render(request, "index.html")
+
+    all_articles = Articles.objects.all()
+
+    context = {
+        "all_articles" : all_articles[:3]
+    }
+
+    return render(request, "index.html", context)
     
 def contact(request):
     return render(request, "contact.html")
