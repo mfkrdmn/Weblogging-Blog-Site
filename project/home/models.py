958cfeb83bea6cb61e5d0879de1e086f91402e2c
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-08-02 16:03:13 +0300

    New blogger request form setup

--- project/home/models.py
+++ project/home/models.py
@@ -1,3 +1,12 @@
+import email
 from django.db import models
 
 # Create your models here.
+
+class New_Blogger(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    reason = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.name
--- project/home/views.py
+++ project/home/views.py
@@ -1,6 +1,7 @@
-from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from articles.models import *
+from .models import *
+from single_page.models import *
 # Create your views here.
 
 
@@ -8,11 +9,28 @@ def home(request):
 
     all_articles = Articles.objects.all()
 
+    all_comments = Comments.objects.all()
+    
+
+    #commented_post a eriştiğin an iş biter
+
     context = {
-        "all_articles" : all_articles[:3]
+        "all_articles" : all_articles[:3],
+        "all_comments" : all_comments[:5],
     }
 
     return render(request, "index.html", context)
+
     
 def contact(request):
+
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        reason = request.POST["reason"]
+
+        newblogger = New_Blogger.objects.create(name=name, email=email, reason=reason)
+        newblogger.save()
+
+
     return render(request, "contact.html")
